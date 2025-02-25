4c1c53cae15ca2503c6dc723249c3bbc892f0b8f
martindevora <martin.devora.pajares@gmail.com>
2025-02-25 16:45:05 +0000

    Preparing release 0.23.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-version = "0.22.2"
+version = "0.23.0"
 setuptools.setup(
     name="lcbuilder", # Replace with your own username
     version=version,

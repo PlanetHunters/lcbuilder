3c545691f3682a10c5650fe51aea50fe1de21b05
martindevora <martin.devora.pajares@gmail.com>
2025-04-22 13:10:09 +0100

    fix bug

--- lcbuilder/objectinfo/preparer/mission_data_preparer.py
+++ lcbuilder/objectinfo/preparer/mission_data_preparer.py
@@ -295,7 +295,7 @@ class EleanorMissionDataPreparer(MissionDataPreparer):
                     self.apertures[s.sector] = ApertureExtractor.from_boolean_mask(datum.aperture.astype(bool),
                                                                               tpf.column, tpf.row)
                     if keep_tpfs:
-                        shutil.copy(tpf.path, self.tpfs_dir + f'/{author}_{cadence}_/' + os.path.basename(tpf.path))
+                        shutil.copy(tpf.path, self.tpfs_dir + f'/{author}_{cadence}_' + os.path.basename(tpf.path))
         quality_bitmask = np.bitwise_and(data[0].quality.astype(int),
                                          object_info.quality_flag if object_info.quality_flag != 'default' else 175)
         self.lc_data = self.extract_eleanor_lc_data(data)

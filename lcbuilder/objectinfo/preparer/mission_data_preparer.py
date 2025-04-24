e60271ab3308515c9445d83740d958b3f8e8c2a7
martindevora <martin.devora.pajares@gmail.com>
2025-04-24 21:09:01 +0100

    update InsetPosition usage from matplotlib for everest daa report

--- lcbuilder/objectinfo/preparer/mission_data_preparer.py
+++ lcbuilder/objectinfo/preparer/mission_data_preparer.py
@@ -3,6 +3,14 @@ import os
 import re
 import shutil
 import sys
+import types
+
+try:
+    from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+except ImportError:
+    dummy = types.ModuleType('inset_locator')
+    dummy.InsetPosition = object
+    sys.modules['mpl_toolkits.axes_grid1.inset_locator'] = dummy
 
 import everest
 import lightkurve as lk
